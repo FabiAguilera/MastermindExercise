a9b529c5f61229165d071eb4d5f4591a50158e2d
Fabiola Aguilera <fabiaguilera96@gmail.com>
2022-11-11 11:38:56 -0500

    created nested If statements for user attempts at game and inner if statement for guessing correct digit

--- Mastermind_Exercise/MasterMind.Repo/Program.cs
+++ Mastermind_Exercise/MasterMind.Repo/Program.cs
@@ -10,7 +10,7 @@ namespace Mastermind_Exercise
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 
@@ -22,17 +22,29 @@ namespace Mastermind_Exercise
             _gameOne.digitOne = 4;
             _gameOne.digitTwo = 1;
             _gameOne.digitThree = 6;
-            _gameOne.digitFour= 5;
+            _gameOne.digitFour = 5;
             _gameOne.gameOneAnswer = int.Parse($"{_gameOne.digitOne}+{_gameOne.digitTwo}+{_gameOne.digitThree}+{_gameOne.digitFour}");
             Console.WriteLine("Let's play Mastermind knockoff!");
             int num = int.Parse(Console.ReadLine());
             for (int i = 1; i <= 6;)
             {
-                if (num == _gameOne.digitOne)
-                {
-
-                }
                 Console.WriteLine($"Enter a number between 1 and 6: {num}");
+                Console.ReadKey();
+                if (num <= 10)
+                {
+                    if (num == _gameOne.digitOne)
+                    {
+                        // outline:
+                        // if correct, then "+"
+                        // if correct but wrong space, then "-"
+                        // if entirely incorrect, then blank space " "
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You've played all of your attempts! Good bye! :)");
+                    break;
+                }
             }
         }
 
