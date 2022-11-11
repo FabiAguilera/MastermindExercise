24b0fb0b60ca0bf5701545e46253ee45d5345782
Fabiola Aguilera <fabiaguilera96@gmail.com>
2022-11-11 11:47:25 -0500

    added notes

--- Mastermind_Exercise/MasterMind.Repo/Program.cs
+++ Mastermind_Exercise/MasterMind.Repo/Program.cs
@@ -16,22 +16,30 @@ namespace Mastermind_Exercise
 
     internal class RandomNum
     {
+        // bring in GuessDigit_01 properties
         public GuessDigit_01 _gameOne = new GuessDigit_01();
+        
+        // setting values to new instance of GuessDigit
         public RandomNum()
         {
             _gameOne.digitOne = 4;
             _gameOne.digitTwo = 1;
             _gameOne.digitThree = 6;
             _gameOne.digitFour = 5;
+
+            // this variable will be used to compare the user input to the answer.
             _gameOne.gameOneAnswer = int.Parse($"{_gameOne.digitOne}+{_gameOne.digitTwo}+{_gameOne.digitThree}+{_gameOne.digitFour}");
             Console.WriteLine("Let's play Mastermind knockoff!");
             int num = int.Parse(Console.ReadLine());
             for (int i = 1; i <= 6;)
             {
-                Console.WriteLine($"Enter a number between 1 and 6: {num}");
+                Console.WriteLine($"Enter four digits between 1 and 6: {num}");
                 Console.ReadKey();
+
+                // first IF for user attempts
                 if (num <= 10)
                 {
+                    // second IF for guessing correct digits
                     if (num == _gameOne.digitOne)
                     {
                         // outline:
