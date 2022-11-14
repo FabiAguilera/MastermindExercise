921606efa2cf280eb625dd7adf5a1d4eb997e4e9
Fabiola Aguilera <fabiaguilera96@gmail.com>
2022-11-13 23:52:46 -0500

    changed nested IF statement to while loop to check if user enters 4 digits, then nested 4 loop to check answer. Created an array to perform operation on individual indexes

--- Mastermind_Exercise/MasterMind.Repo/Program.cs
+++ Mastermind_Exercise/MasterMind.Repo/Program.cs
@@ -18,7 +18,7 @@ namespace Mastermind_Exercise
     {
         // bring in GuessDigit_01 properties
         public GuessDigit_01 _gameOne = new GuessDigit_01();
-        
+
         // setting values to new instance of GuessDigit
         public RandomNum()
         {
@@ -26,34 +26,50 @@ namespace Mastermind_Exercise
             _gameOne.digitTwo = 1;
             _gameOne.digitThree = 6;
             _gameOne.digitFour = 5;
+            List<int> result = new List<int>();
 
             // this variable will be used to compare the user input to the answer.
-            _gameOne.gameOneAnswer = int.Parse($"{_gameOne.digitOne}+{_gameOne.digitTwo}+{_gameOne.digitThree}+{_gameOne.digitFour}");
-            Console.WriteLine("Let's play Mastermind knockoff!");
-            int num = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= 6;)
-            {
-                Console.WriteLine($"Enter four digits between 1 and 6: {num}");
-                Console.ReadKey();
+            //_gameOne.gameOneAnswer[] = ToString().Select(t => int.Parse(t.ToString())).ToArray();
+            //int.Parse($"{_gameOne.digitOne}+{_gameOne.digitTwo}+{_gameOne.digitThree}+{_gameOne.digitFour}");
 
-                // first IF for user attempts
-                if (num <= 10)
+            Console.WriteLine("Let's play Mastermind console app!");
+
+            Console.WriteLine($"Enter four digits between 1 and 6: ");
+
+            int num = int.Parse(Console.ReadLine());
+            while (Console.ReadLine().Length <= 4)
+            {
+                //num = int.Parse(Console.ReadLine());
+                if(num == _gameOne.gameOneAnswer)
                 {
-                    // second IF for guessing correct digits
-                    if (num == _gameOne.digitOne)
-                    {
-                        // outline:
-                        // if correct, then "+"
-                        // if correct but wrong space, then "-"
-                        // if entirely incorrect, then blank space " "
-                    }
+                    Console.WriteLine($"You've guessed the correct four digit combination! {_gameOne.gameOneAnswer}");
                 }
                 else
                 {
-                    Console.WriteLine("You've played all of your attempts! Good bye! :)");
-                    break;
+                    if (Convert.ToBoolean(num.CompareTo(_gameOne.digitOne)))
+                    {
+
+                    }
+                }
+
+            }
+
+            /*
+            
+                // second IF for guessing correct digits
+                if (num == _gameOne.digitOne)
+                {
+                    // outline:
+                    // if correct, then "+"
+                    // if correct but wrong space, then "-"
+                    // if entirely incorrect, then blank space " "
                 }
             }
+            else
+            {
+                Console.WriteLine("You've played all of your attempts! Good bye! :)");
+            }*/
+
         }
 
     }
