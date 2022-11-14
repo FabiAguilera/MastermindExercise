11b70e4cc5b705a704251ff347609c9030905086
Fabiola Aguilera <fabiaguilera96@gmail.com>
2022-11-14 00:13:26 -0500

    removed list of List<int> and replaced with int[]. Added if elseif statements to check user input correctness of individual index of array.

--- Mastermind_Exercise/MasterMind.Repo/Program.cs
+++ Mastermind_Exercise/MasterMind.Repo/Program.cs
@@ -26,49 +26,68 @@ namespace Mastermind_Exercise
             _gameOne.digitTwo = 1;
             _gameOne.digitThree = 6;
             _gameOne.digitFour = 5;
-            List<int> result = new List<int>();
+            //List<int> result = new List<int>();
 
             // this variable will be used to compare the user input to the answer.
-            //_gameOne.gameOneAnswer[] = ToString().Select(t => int.Parse(t.ToString())).ToArray();
-            //int.Parse($"{_gameOne.digitOne}+{_gameOne.digitTwo}+{_gameOne.digitThree}+{_gameOne.digitFour}");
+            _gameOne.gameOneAnswer = int.Parse($"{_gameOne.digitOne}+{_gameOne.digitTwo}+{_gameOne.digitThree}+{_gameOne.digitFour}");
 
-            Console.WriteLine("Let's play Mastermind console app!");
+            Console.WriteLine("Let's play Mastermind console app! In this game, you will attempt to guess what the four digit combination is. A plus (+) sign indicates the number you guessed is the correct digit in the correct space, a minus (-) for the correct digit in the wrong place, and a blank space for incorrect digit and incorrect space. Let's begin!");
 
             Console.WriteLine($"Enter four digits between 1 and 6: ");
-
-            int num = int.Parse(Console.ReadLine());
+            int result = int.Parse(Console.ReadLine());
+            int[] num = Array.ConvertAll(result.ToString("0").ToCharArray(), ch => ch - '0');
             while (Console.ReadLine().Length <= 4)
             {
-                //num = int.Parse(Console.ReadLine());
-                if(num == _gameOne.gameOneAnswer)
+                if (Convert.ToBoolean(num[0] == _gameOne.digitOne))
                 {
-                    Console.WriteLine($"You've guessed the correct four digit combination! {_gameOne.gameOneAnswer}");
+                    Console.WriteLine("+");
                 }
-                else
+                else if (Convert.ToBoolean(num[1] == _gameOne.digitTwo))
                 {
-                    if (Convert.ToBoolean(num.CompareTo(_gameOne.digitOne)))
-                    {
-
-                    }
+                    Console.WriteLine("+");
+                }
+                else if (Convert.ToBoolean(num[2] == _gameOne.digitThree))
+                {
+                    Console.WriteLine("+");
+                }
+                else if (Convert.ToBoolean(num[3] == _gameOne.digitFour))
+                {
+                    Console.WriteLine("+");
+                }
+                if (Convert.ToBoolean(num[0] == _gameOne.digitOne))
+                {
+                    Console.WriteLine("-");
+                }
+                else if (Convert.ToBoolean(num[1] == _gameOne.digitTwo))
+                {
+                    Console.WriteLine("-");
+                }
+                else if (Convert.ToBoolean(num[2] == _gameOne.digitThree))
+                {
+                    Console.WriteLine("-");
+                }
+                else if (Convert.ToBoolean(num[3] == _gameOne.digitFour))
+                {
+                    Console.WriteLine("-");
+                }
+                else if (Convert.ToBoolean(num[0] != _gameOne.digitOne))
+                {
+                    Console.WriteLine(" ");
+                }
+                else if (Convert.ToBoolean(num[1] != _gameOne.digitTwo))
+                {
+                    Console.WriteLine(" ");
+                }
+                else if (Convert.ToBoolean(num[2] != _gameOne.digitThree))
+                {
+                    Console.WriteLine(" ");
+                }
+                else if (Convert.ToBoolean(num[3] != _gameOne.digitFour))
+                {
+                    Console.WriteLine(" ");
                 }
 
             }
-
-            /*
-            
-                // second IF for guessing correct digits
-                if (num == _gameOne.digitOne)
-                {
-                    // outline:
-                    // if correct, then "+"
-                    // if correct but wrong space, then "-"
-                    // if entirely incorrect, then blank space " "
-                }
-            }
-            else
-            {
-                Console.WriteLine("You've played all of your attempts! Good bye! :)");
-            }*/
 
         }
 
